cedd11112e5b8a552ce219c3961b7b2b4212f4fc
ewkimwaki <e.w.kimwaki@gmail.com>
2024-02-05 08:56:36 +0300

    Assignment complete

---
+++ src/components/Question.js
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from "react";
+
+function Question({ question, onAnswered }) {
+  const [timeRemaining, setTimeRemaining] = useState(10);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setTimeRemaining(prevTime => prevTime - 1);
+    }, 1000);
+
+    return () => clearTimeout(timer);
+  }, [timeRemaining]);
+
+  useEffect(() => {
+    if (timeRemaining === 0) {
+      setTimeRemaining(10);
+      onAnswered(false);
+    }
+  }, [timeRemaining, onAnswered]);
+
+  function handleAnswer(isTrue) {
+    setTimeRemaining(10);
+    onAnswering(isTrue);
+  }
+
+  const { id, prompt, answers, correctTimer} = question;
+
+  return (
+    <>
+      <h1>Question {id}</h1>
+      <h3>{prompt}</h3>
+      {answers.map((answer, timer) => {
+        const isTrue = timer === correctTimer;
+        return (
+          <button key={answer} onClick={() => handleAnswer(isTrue)}>
+            {answer}
+          </button>
+        );
+      })}
+      <h5>{timeRemaining} seconds remaining</h5>
+    </>
+  );
+}
+
+export default Question;
